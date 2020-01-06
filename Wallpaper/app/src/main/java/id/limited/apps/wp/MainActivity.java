25715522bf66f03bf2c38c46a8d62e9a1b8dc796
maskaywalanay <mwalanay@gmail.com>
2020-01-06 21:32:01 +0700

    first commit

---
+++ Wallpaper/app/src/main/java/id/limited/apps/wp/MainActivity.java
@@ -0,0 +1,15 @@
+package id.limited.apps.wp;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+	// first commit
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
