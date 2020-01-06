e1db6b90cd96af04c6029804405dffb00da0e8f1
maskaywalanay <mwalanay@gmail.com>
2020-01-07 00:45:22 +0700

    Third commit

---
+++ Wallpaper/app/src/main/java/id/limited/apps/wp/GradientSplash.java
@@ -0,0 +1,29 @@
+package id.limited.apps.wp;
+import android.view.*;
+import android.content.*;
+import android.util.*;
+
+public class GradientSplash extends View
+{
+	
+	static final String RED = "#FF0000";
+	static final String GREEN = "#FF00FF00";
+	static final String BLUE = "#FF0000FF";
+	
+	
+	public GradientSplash(Context c, AttributeSet attr, int defStyle){
+		super(c, attr, defStyle);
+		
+	}
+
+	
+	
+	public GradientSplash(Context c, AttributeSet attr){
+		super(c, attr);
+	}
+	
+	public GradientSplash(Context c){
+		super(c);
+		
+	}
+}
--- Wallpaper/app/src/main/java/id/limited/apps/wp/MainActivity.java
+++ Wallpaper/app/src/main/java/id/limited/apps/wp/MainActivity.java
@@ -2,6 +2,7 @@ package id.limited.apps.wp;
 
 import android.os.*;
 import android.support.v7.app.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity 
 {
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity
 	void create(){
 		String s = "senin 6 januari 2020";
 	}
+	
 	// first commit
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
+		Toast.makeText(getApplicationContext(),R.string.app_toast_a,Toast.LENGTH_SHORT).show();
 		
 		
 		
